5f202c770ae560de285c0f88b0adb134e65f0de7
kellycassis <129323717+kellycassis@users.noreply.github.com>
2023-03-30 14:41:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+
+import './App.css';
+import Video from './pages/Video';
+import video from "./pages/Video"
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app_videos'> 
+        <Video/>
+        <Video/>
+        <Video/>
+      
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
